e6ebf7d21dd23f35bfcabdaf0317f7d97532af34
Luis <luispadron25@gmail.com>
2025-06-10 19:43:45 -0500

    add: reload command

---
+++ src/commands/dev/reload.js
@@ -0,0 +1,54 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('reload')
+    .setDescription('Reloads a command.')
+    .addStringOption((option) =>
+      option
+        .setName('command')
+        .setDescription('The command to reload.')
+        .setRequired(true),
+    ),
+  async execute(interaction) {
+    const { user } = interaction;
+
+    if (user.id !== '140528330456825856') {
+      return interaction.reply({
+        content: 'Command only for developers',
+        ephemeral: true,
+      });
+    }
+
+    const commandName = interaction.options
+      .getString('command', true)
+      .toLowerCase();
+    const command = interaction.client.commands.get(commandName);
+
+    if (!command) {
+      return interaction.reply(
+        `There is no command with name \`${commandName}\`!`,
+      );
+    }
+
+    delete require.cache[
+      require.resolve(`../${command.category}/${command.data.name}.js`)
+    ];
+
+    try {
+      interaction.client.commands.delete(command.data.name);
+      const newCommand = require(
+        `../${command.category}/${command.data.name}.js`,
+      );
+      interaction.client.commands.set(newCommand.data.name, newCommand);
+      await interaction.reply(
+        `Command \`${newCommand.data.name}\` was reloaded!`,
+      );
+    } catch (error) {
+      console.error(error);
+      await interaction.reply(
+        `There was an error while reloading a command \`${command.data.name}\`:\n\`${error.message}\``,
+      );
+    }
+  },
+};
